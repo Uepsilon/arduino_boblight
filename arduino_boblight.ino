ef5077b1414686c1db15cbe64d865cf9d1f06ed6
Uepsilon <Maxim.Kuepper@gmail.com>
2013-03-24 19:24:57 +0100

    added example boblight config; reading values from boblight

--- arduino_boblight.ino
+++ arduino_boblight.ino
@@ -1,74 +1,88 @@
 #include <SoftPWM.h>
 
+// FADING SETTINGS
+#define FADE_IN_TIME    50
+#define FADE_OUT_TIME   50
+
+// Channel 1
+#define RED1    2
+#define GREEN1  3
+#define BLUE1   4
+
+// Channel 2
+#define RED2    5
+#define GREEN2  6
+#define BLUE2   7
+
+// Channel 3
+#define RED3    8
+#define GREEN3  9
+#define BLUE3   10
+
+// Channel 4
+#define RED4    11
+#define GREEN4  12
+#define BLUE4   13
+
+/*
+ * --------------------------------
+ *  THIS IS WHAT YOU HAVE TO EDIT !
+ * --------------------------------
+ */
+#define ACTIVE_CHANNELS 3
+/*
+ * --------------------------------
+ * HERE ENDS WHAT YOU HAVE TO EDIT!
+ * --------------------------------
+ */
+
+// each channel exists of 3 colors
+#define COLORS_USED (ACTIVE_CHANNELS * 3)
+
+uint8_t values[COLORS_USED];
+uint8_t outputs[] = {RED1, GREEN1, BLUE1, RED2, GREEN2, BLUE2, RED3, GREEN3, BLUE3, RED4, GREEN4, BLUE4};
+
 void setup()
 {
   // Initialize
   SoftPWMBegin();
+  Serial.begin(38400);
 
   // Create and set pin 13 to 0 (off)
-  SoftPWMSet(2, 0);
-  SoftPWMSet(3, 0);
-  SoftPWMSet(4, 0);
+  for(uint8_t i = 0; i < sizeof(outputs); i++)
+  {
+    // Turn off all outputs
+    SoftPWMSet(outputs[i], 0);
 
-  SoftPWMSet(5, 0);
-  SoftPWMSet(6, 0);
-  SoftPWMSet(7, 0);
+    // set fade time
+    SoftPWMSetFadeTime(outputs[i], FADE_IN_TIME, FADE_OUT_TIME);
+  }
+}
 
-  SoftPWMSet(11, 0);
-  SoftPWMSet(12, 0);
-  SoftPWMSet(13, 0);
-
-  // Set fade time for pin 13 to 100 ms fade-up time, and 500 ms fade-down time
-  SoftPWMSetFadeTime(2, 1500, 2000);
-  SoftPWMSetFadeTime(3, 1500, 2000);
-  SoftPWMSetFadeTime(4, 1500, 2000);
-
-  SoftPWMSetFadeTime(5, 1500, 2000);
-  SoftPWMSetFadeTime(6, 1500, 2000);
-  SoftPWMSetFadeTime(7, 1500, 2000);
-
-  SoftPWMSetFadeTime(11, 1500, 2000);
-  SoftPWMSetFadeTime(12, 1500, 2000);
-  SoftPWMSetFadeTime(13, 1500, 2000);
+//boblightd needs to send 0x55 0xAA before sending the channel bytes
+void WaitForPrefix()
+{
+  uint8_t first = 0, second = 0;
+  while (second != 0x55 || first != 0xAA)
+  {
+    while (!Serial.available());
+    second = first;
+    first = Serial.read();
+  }
 }
 
 void loop()
 {
-  // Turn on - set to 100%
-  SoftPWMSetPercent(2, 100);
-  SoftPWMSetPercent(5, 100);
-  SoftPWMSetPercent(11, 100);
+  WaitForPrefix();
 
-  // Wait for LED to turn on - you could do other tasks here
-  delay(2000);
+  for (uint8_t i = 0; i < COLORS_USED; i++)
+  {
+    while(!Serial.available());
+    values[i] = Serial.read();
+  }
 
-  // Turn off - set to 0%
-  SoftPWMSetPercent(2, 0);
-  SoftPWMSetPercent(5, 0);
-  SoftPWMSetPercent(11, 0);
-
-  SoftPWMSetPercent(3, 100);
-  SoftPWMSetPercent(6, 100);
-  SoftPWMSetPercent(12, 100);
-
-  // Wait for LED to turn on - you could do other tasks here
-  delay(2000);
-
-  // Turn off - set to 0%
-  SoftPWMSetPercent(3, 0);
-  SoftPWMSetPercent(6, 0);
-  SoftPWMSetPercent(12, 0);
-
-  SoftPWMSetPercent(4, 100);
-  SoftPWMSetPercent(7, 100);
-  SoftPWMSetPercent(13, 100);
-
-  // Wait for LED to turn off
-  delay(2000);
-
-  SoftPWMSetPercent(4, 0);
-  SoftPWMSetPercent(7, 0);
-  SoftPWMSetPercent(13, 0);
-
-  delay(2000);
+  for (uint8_t i = 0; i < COLORS_USED; i++)
+  {
+    SoftPWMSet(outputs[i], values[i]);
+  }
 }
